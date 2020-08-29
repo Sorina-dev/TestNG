e54317c13c5923c83912deca5f4d3354ddc9e8c9
Sorina-dev <cristiansorina@gmail.com>
2020-08-29 18:26:48 +0300

    TestNG intro

---
+++ src/com/syntax/class01/TestNGIntro.java
@@ -0,0 +1,19 @@
+package com.syntax.class01;
+
+import org.testng.annotations.Test;
+
+public class TestNGIntro {
+    
+	@Test
+	public void testOne() {
+		System.out.println("I am test one");
+	}
+	@Test
+	public void testTwo() {
+		System.out.println("I am test 2");
+	}
+	@Test
+	public void testThree() {
+		System.out.println("I am test 3");
+	}
+}
---
+++ src/com/syntax/class01/TestNGTest.java
@@ -0,0 +1,10 @@
+package com.syntax.class01;
+
+import org.testng.annotations.Test;
+
+public class TestNGTest {
+	@Test
+	public void hello() {
+		System.out.println("Hello");
+	}
+}
